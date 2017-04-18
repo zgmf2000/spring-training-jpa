bbdb68e93ebf83c2720fbbc0997bd62df486df96
Bintoro Adi Guna <trainee@MTPC216.mitrais.com>
2017-04-18 15:53:58 +0700

    Added a new PagingAndSortingRepository method

--- src/main/java/com/spring/jpa/EmployeeRepository.java
+++ src/main/java/com/spring/jpa/EmployeeRepository.java
@@ -2,12 +2,15 @@ package com.spring.jpa;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee,Long>
 {
     public List<Employee> findByLastName(@Param("last_name") String name);
+
+    public List<Employee> findByLastNameContainingIgnoreCase(@Param("last_name") String name);
 
     public List<Employee> findByGender(@Param("gender") String gender);
 }
