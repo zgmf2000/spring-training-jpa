2d9d6c82d88dab590a709ab08dc005dc40ac14ed
Bintoro Adi Guna <trainee@MTPC216.mitrais.com>
2017-04-20 08:31:45 +0700

    Added a method to return ResponseEntity object.

--- src/main/java/com/spring/jpa/EmployeeController.java
+++ src/main/java/com/spring/jpa/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.spring.jpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by trainee on 18/04/2017.
@@ -13,9 +18,17 @@ import java.util.AbstractMap;
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeRepository repository;
+
     @RequestMapping(value = "employees/{empId}/score", method = RequestMethod.GET)
     AbstractMap.SimpleEntry<String, Double> getEmployeeByFilter(@PathVariable long empId) {
         double result = Math.random() * 100;
         return new AbstractMap.SimpleEntry<String, Double>("score", result);
+    }
+
+    @RequestMapping(value = "employees/{empId}/experiment", method = RequestMethod.GET)
+    ResponseEntity<Employee> getEmployees(@PathVariable long empId) {
+        return new ResponseEntity<>(repository.findOne(empId), HttpStatus.OK);
     }
 }
